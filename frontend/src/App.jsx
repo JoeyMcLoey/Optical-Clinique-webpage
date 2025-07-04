c5fc84e9d78734e34c4d234c9ce009833b0302f2
Joseph Kizana <joseph.k20002@gmail.com>
2025-07-04 23:18:50 +1000

    minor changes to navbar, now sticks to the top

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,9 @@ function App() {
   return (
       <Box width="100vw">
         <Logo />
-        <Navbar />
+        <Box position="sticky" top="0" zIndex="999" bg="white" paddingTop="5" paddingBottom="5">
+          <Navbar />
+        </Box>
         <Routes>
           <Route path="/" element={<HomePage />} />
         </Routes>
