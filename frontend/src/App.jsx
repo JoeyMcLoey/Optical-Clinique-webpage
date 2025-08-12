f0d5eed05fac017b5acea9ed43c65ba8ce786844
Joseph Kizana <joseph.k20002@gmail.com>
2025-08-12 15:48:21 +1000

    button colours functional now

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Box, ChakraProvider } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import { Route, Routes } from 'react-router-dom';
 import HomePage from "./pages/HomePage";
 import Navbar from "./components/Navbar";
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,5 @@
-import { Provider } from "@/components/ui/provider"
+// import { Provider } from "@/components/ui/provider"
+import { ChakraProvider } from "@chakra-ui/react"
 import React from "react"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
@@ -8,9 +9,9 @@ import { BrowserRouter } from "react-router-dom"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <Provider>
+      <ChakraProvider>
         <App />
-      </Provider>
+      </ChakraProvider>
     </BrowserRouter>
   </StrictMode>,
 )
